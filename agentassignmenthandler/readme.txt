e91bfe97988f48445b6514f7be68f83aa3ea4456
madhusudan gohil <madhusudan.gohil@incontact.com>
2017-08-17 12:19:16 -0400

    credentials

--- agentassignmenthandler/readme.txt
+++ agentassignmenthandler/readme.txt
@@ -36,3 +36,24 @@ output
 aws lambda create-event-source-mapping --event-source-arn arn:aws:sns:us-west-2:459375513878:Message-Queued --function-name agent-assignment-handler
 
 aws sns subscribe --topic-arn arn:aws:sns:us-west-2:459375513878:Message-Queued --protocol lambda --notification-endpoint arn:aws:lambda:us-west-2:459375513878:function:agent-assignment-handler
+
+
+//how to set credentials in javascrip code.
+const credentials = new AWS.SharedIniFileCredentials({ profile: 'dev' });
+AWS.config.credentials = credentials;
+
+//how to assume role
+var params = {
+    RoleArn: 'arn:aws:iam::300813158921:role/GroupAccess-Developers-Channels', /* required */
+    RoleSessionName: 'test'
+};
+let sts = new AWS.STS();
+sts.assumeRole(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
+});
+
+//how to set temporary credentials, along with assumed role.
+AWS.config.credentials = new AWS.TemporaryCredentials({
+    RoleArn: process.env.ASSUMED_ROLE_ARN
+},credentials);
--- apigateway/apigateway.ps1
+++ apigateway/apigateway.ps1
@@ -14,46 +14,54 @@ $accountId = "123456789"
 $resourceName = "helloWorld"
 $lambdaFunctionName = "channels-inbound-message-handler"
 $profile = "individual"
+$role = "sms_prototype"
+$handlerMethod = "inboundMessageHandler.handler"
+$apiName = "helloWorldApi"
 
 $lambdaArn = "arn:aws:lambda:$($region):$($accountId):function:$($lambdaFunctionName)"
+$roleArn = "arn:aws:iam::$($accountId):role/$($role)"
+$apiGateWayUriArn = "arn:aws:apigateway:$($region):lambda:path/2015-03-31/functions/arn:aws:lambda:$($region):$($accountId):function:$($lambdaFunctionName)/invocations"
 
-
+Write-Output "Removing Lambda"
 aws lambda delete-function `
 --function-name $lambdaArn `
 --region $region `
 --profile $profile
+Write-Output "Lambda Removed"
 
+Write-Output "Create Lambda"
 aws lambda create-function --function-name   channels-inbound-message-handler  `
 --zip-file fileb://../lambdas/inboundMessageHandler.zip `
---role arn:aws:iam::123456789:role/sms_prototype --handler inboundMessageHandler.handler `
---runtime nodejs6.10 --region us-west-2 `
---profile individual
+--role $roleArn --handler $handlerMethod `
+--runtime nodejs6.10 --region $region `
+--profile $profile
+Write-Output "Lambda Created"
 Write-Output "Lambda Deployment Done"
 
 Write-Output "Creating Api"
-$restApi = aws apigateway create-rest-api --name helloWorldApi --description 'test cli for api creation' `
---profile individual --region us-west-2 | ConvertFrom-Json
+$restApi = aws apigateway create-rest-api --name $apiName --description 'test cli for api creation' `
+--profile $profile --region $region | ConvertFrom-Json
 Write-Output "Rest Api Created with id $($restApi.id)"
 
 Write-Output "Getting resource for newly created Api"
-$parent = aws apigateway get-resources --rest-api-id $restApi.id --profile individual --region us-west-2 | ConvertFrom-Json
+$parent = aws apigateway get-resources --rest-api-id $restApi.id --profile $profile --region $region | ConvertFrom-Json
 Write-Output "Parent Id is  $($parent.items[0].id)"
 
 Write-Output "Create Resource"
 $resource = aws apigateway create-resource  --rest-api-id $restApi.id `
---parent-id $parent.items[0].id --path-part 'helloWorld' --profile individual --region us-west-2 | ConvertFrom-Json
+--parent-id $parent.items[0].id --path-part $resourceName --profile $profile --region $region | ConvertFrom-Json
 Write-Output $resource
 Write-Output "Resource Created $($resource.id)"
 
 Write-Output "Registering Put Method"
 aws apigateway put-method --rest-api-id $restApi.id --resource-id $resource.id `
---http-method POST --authorization-type NONE --region us-west-2 --profile individual
+--http-method POST --authorization-type NONE --profile $profile --region $region 
 Write-Output "Put Method Done"
 
 Write-Output "Registering Put Integration"
 aws apigateway put-integration --rest-api-id $restApi.id --resource-id $resource.id `
 --http-method POST --type AWS --integration-http-method POST `
---uri arn:aws:apigateway:us-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-west-2:123456789:function:channels-inbound-message-handler/invocations `
+--uri $apiGateWayUriArn `
 --request-templates '{\"application/x-www-form-urlencoded\":\""#set($httpPost=$input.path(\\\"$\\\").split(\\\"&\\\"))\r\n{#foreach($kvPair\u0020in\u0020$httpPost)\r\n#set($kvTokenised=$kvPair.split(\\\"=\\\"))\r\n#if($kvTokenised.size()>1)\r\n\\\"$kvTokenised[0]\\\":\\\"$kvTokenised[1]\u0020\\\"\r\n#if($foreach.hasNext),#end\u0020\r\n#else\u0020\\\"$kvTokenised[0]\\\":\\\"\\\"\r\n#if($foreach.hasNext),#end\r\n#end\r\n#end}"\"}' `
 --profile individual `
 --region us-west-2
@@ -65,18 +73,18 @@ $method = "POST"
 $sourceArn = "arn:aws:execute-api:$($region):$($accountId):$($restApi.id)/$($stage)/$($method)/$($resourceName)"
 
 aws lambda add-permission --function-name channels-inbound-message-handler `
---statement-id apigateway-helloworld-4 `
+--statement-id apigateway-helloworld-5 `
 --action lambda:InvokeFunction `
 --principal 'apigateway.amazonaws.com' `
 --source-arn $sourceArn `
---region us-west-2 --profile individual
+--region $region --profile $profile
 
 Write-Output "Deploying Api"
 aws apigateway  create-deployment --rest-api-id $restApi.id `
---stage-name 'prod' --profile individual --region us-west-2
+--stage-name 'prod' --profile $profile --region $region
 Write-Output "Api Deployed"
 
 Write-Output "Delete Resource  $($restApi.id)"
-#aws apigateway delete-rest-api --rest-api-id $restApi.id --profile individual
+aws apigateway delete-rest-api --rest-api-id $restApi.id --profile $profile --region $region
 Write-Output "Resource Deleted"
 
